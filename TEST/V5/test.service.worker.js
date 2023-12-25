363801a940b121bd4bb5ca720fb970404a3fd559
albarroalo <alvaroaq@outlook.es>
2023-12-26 00:31:54 +0100

    Testeo de uso de threads

--- TEST/V5/test.service.worker.js
+++ TEST/V5/test.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1703544291|271118694";
+const CACHE_VERSION = "1703547055|3035280756";
 const CACHE_PREFIX = "Free Caaar-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "test.offline.html";
